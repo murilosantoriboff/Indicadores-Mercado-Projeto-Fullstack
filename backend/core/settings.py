476bfabfea891058e591c3e9d388cca716f353b7
Murilo Santori Boff <murilosantori45@gmail.com>
2026-01-09 09:31:58 -0300

    feat: configure static files settings for production

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -162,7 +162,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = []
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
