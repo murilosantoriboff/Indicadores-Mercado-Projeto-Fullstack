fdd7bfc1fc2ed791d1aa6f1367db8c3aeebf2c1a
Murilo Santori Boff <murilosantori45@gmail.com>
2026-01-09 09:57:02 -0300

    feat: update ALLOWED_HOSTS and add CSRF_TRUSTED_ORIGINS configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False, cast=bool)
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
@@ -98,7 +97,11 @@ CORS_ALLOWED_ORIGINS = config(
     default='http://localhost:3000',
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
-
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://localhost:3000',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
